afe6ab6e9d30a43db0379358804b77b54459a54f
wofish2 <donghaiy@jp.ibm.com>
2018-05-24 15:57:16 +0900

    Update microserviceBuilderPipeline.groovy

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -210,10 +210,10 @@ def call(body) {
               sh buildCommand
               if (registry) {
                 //
-		retry(5){
+		//retry(5){
 		     sh "docker login mycluster.icp:8500 -u ${reguser} -p ${regpw}"
-		}
-		sh "cat /home/jenkins/.docker/config.json"
+		//}
+		//sh "cat /home/jenkins/.docker/config.json"
 		
                 sh "docker tag ${image}:${imageTag} ${registry}${image}:${imageTag}"
                 sh "docker push ${registry}${image}:${imageTag}"
